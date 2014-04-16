f22ecef7205e59d0d4722bc5b832348c91a56b9f
wsanchez <wsanchez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-04-16 18:42:22 +0000

    Merge forward
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/log-booyah-6750-4@42402 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/python/logger/_json.py
@@ -0,0 +1,226 @@
+# -*- test-case-name: twisted.python.logger.test.test_json -*-
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+"""
+Tools for saving and loading log events in a structured format.
+"""
+
+import types
+from json import dumps, loads
+from uuid import UUID
+
+from ._flatten import flattenEvent
+from ._file import FileLogObserver
+from ._levels import LogLevel
+from twisted.python.constants import NamedConstant
+
+from twisted.python.compat import unicode
+from twisted.python.failure import Failure
+
+
+
+def failureAsJSON(obj):
+    """
+    Convert a L{Failure} to a JSON-serializable data structure.
+
+    @return: a L{dict} of L{str} to ...  stuff, mostly reminiscent of
+        L{Failure.__getstate__}
+    """
+    return dict(
+        obj.__getstate__(),
+        type=dict(
+            __module__=obj.type.__module__,
+            __name__=obj.type.__name__,
+        )
+    )
+
+
+
+def nativify(x):
+    """
+    On Python 2, we really need native strings in a variety of places;
+    attribute names will sort of work in a __dict__, but they're subtly wrong;
+    however, printing tracebacks relies on I/O to containers that only support
+    bytes.  This function converts _all_ native strings within a
+    JSON-deserialized object to bytes.
+    """
+    if isinstance(x, list):
+        return map(nativify, x)
+    elif isinstance(x, dict):
+        return dict((nativify(k), nativify(v)) for k, v in x.items())
+    elif isinstance(x, unicode):
+        return x.encode("utf-8")
+    else:
+        return x
+
+
+
+def failureFromJSON(failureDict):
+    """
+    Load a L{Failure} from a dictionary deserialized from JSON.
+
+    @param failureDict: a JSON-deserialized object like one previously returned
+        by L{failureAsJSON}.
+    @type failureDict: L{dict} mapping L{unicode} to attributes
+
+    @return: L{Failure}
+    @rtype: L{Failure}
+    """
+    newFailure = getattr(Failure, "__new__", None)
+    if newFailure is None:
+        failureDict = nativify(failureDict)
+        f = types.InstanceType(Failure)
+    else:
+        f = newFailure(Failure)
+    typeInfo = failureDict["type"]
+    failureDict["type"] = type(typeInfo["__name__"], (), typeInfo)
+    f.__dict__ = failureDict
+    return f
+
+
+
+classInfo = [
+    (
+        lambda level: (
+            isinstance(level, NamedConstant) and
+            getattr(LogLevel, level.name, None) is level
+        ),
+        UUID("02E59486-F24D-46AD-8224-3ACDF2A5732A"),
+        lambda level: dict(name=level.name),
+        lambda level: getattr(LogLevel, level["name"], None)
+    ),
+
+    (
+        lambda o: isinstance(o, Failure),
+        UUID("E76887E2-20ED-49BF-A8F8-BA25CC586F2D"),
+        failureAsJSON, failureFromJSON
+    ),
+]
+
+
+
+uuidToLoader = dict([
+    (uuid, loader) for (predicate, uuid, saver, loader) in classInfo
+])
+
+
+
+def objectLoadHook(aDict):
+    """
+    Dictionary-to-object-translation hook for certain value types used within
+    the logging system.
+
+    @see: the C{object_hook} parameter to L{json.load}
+
+    @param aDict: A dictionary loaded from a JSON object.
+    @type aDict: L{dict}
+
+    @return: L{aDict} itself, or the object represented by L{aDict}
+    @rtype: L{object}
+    """
+    if "__class_uuid__" in aDict:
+        return uuidToLoader[UUID(aDict["__class_uuid__"])](aDict)
+    return aDict
+
+
+
+def objectSaveHook(pythonObject):
+    """
+    Object-to-serializable hook for certain value types used within the logging
+    system.
+
+    @see: the C{default} parameter to L{json.dump}
+
+    @param pythonObject: Any object.
+    @type pythonObject: L{object}
+
+    @return: If the object is one of the special types the logging system
+        supports, a specially-formatted dictionary; otherwise, a marker
+        dictionary indicating that it could not be serialized.
+    """
+    for (predicate, uuid, saver, loader) in classInfo:
+        if predicate(pythonObject):
+            result = saver(pythonObject)
+            result["__class_uuid__"] = str(uuid)
+            return result
+    return {"unpersistable": True}
+
+
+
+def eventAsJSON(event):
+    """
+    Encode an event as JSON, flattening it if necessary to preserve as much
+    structure as possible.
+
+    Not all structure from the log event will be preserved when it is
+    serialized
+
+    @param event: A log event dictionary.
+    @type event: L{dict} with arbitrary keys and values
+
+    @return: A string of the serialized JSON; note that this will contain no
+        newline characters, and may thus safely be stored in a line-delimited
+        file.
+    @rtype: L{unicode}
+    """
+    if bytes is str:
+        kw = dict(default=objectSaveHook, encoding="charmap", skipkeys=True)
+    else:
+        def default(unencodable):
+            if isinstance(unencodable, bytes):
+                return unencodable.decode("charmap")
+            return objectSaveHook(unencodable)
+        kw = dict(default=default, skipkeys=True)
+    flattenEvent(event)
+    result = dumps(event, **kw)
+    if not isinstance(result, unicode):
+        return unicode(result, "utf-8", "replace")
+    return result
+
+
+
+def eventFromJSON(eventText):
+    """
+    Decode a log event from JSON.
+
+    @param eventText: The output of a previous call to L{eventAsJSON}
+    @type eventText: L{unicode}
+
+    @return: A reconstructed version of the log event.
+    @rtype: L{dict}
+    """
+    loaded = loads(eventText, object_hook=objectLoadHook)
+    return loaded
+
+
+
+def jsonFileLogObserver(outFile):
+    """
+    Create a L{FileLogObserver} that emits JSON lines to a specified (writable)
+    file-like object.
+
+    @param outFile: A file-like object.  Ideally one should be passed which
+        accepts L{unicode} data.  Otherwise, UTF-8 L{bytes} will be used.
+    @type outFile: L{io.IOBase}
+
+    @return: A file log observer.
+    @rtype: L{FileLogObserver}
+    """
+    return FileLogObserver(outFile, lambda event: eventAsJSON(event) + u"\n")
+
+
+
+def eventsFromJSONLogFile(inFile):
+    """
+    Load events from a file previously saved with L{jsonFileLogObserver}.
+
+    @param inFile: A (readable) file-like object.  Data read from L{inFile}
+        should be L{unicode} or UTF-8 L{bytes}.
+    @type inFile: iterable of lines
+
+    @return: Log events as read from C{inFile}.
+    @rtype: iterable of L{dict}
+    """
+    for line in inFile:
+        yield eventFromJSON(line)
---
+++ twisted/python/logger/test/test_flatten.py
@@ -0,0 +1,225 @@
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+"""
+Test cases for L{twisted.python.logger._format}.
+"""
+
+import sys
+from itertools import count
+import json
+
+try:
+    from time import tzset
+    # We should upgrade to a version of pyflakes that does not require this.
+    tzset
+except ImportError:
+    tzset = None
+
+from twisted.trial import unittest
+
+from .._format import formatEvent
+from .._flatten import (
+    flattenEvent, extractField, KeyFlattener, aFormatter
+)
+
+
+
+class FlatFormattingTests(unittest.TestCase):
+    """
+    Tests for flattened event formatting functions.
+    """
+
+    def test_formatFlatEvent(self):
+        """
+        L{flattenEvent} will "flatten" an event so that, if scrubbed of all but
+        serializable objects, it will preserve all necessary data to be
+        formatted once serialized.  When presented with an event thusly
+        flattened, L{formatEvent} will produce the same output.
+        """
+        counter = count()
+
+        class Ephemeral(object):
+            attribute = "value"
+
+        event1 = dict(
+            log_format="callable: {callme()} attribute: {object.attribute} "
+                       "numrepr: {number!r} strrepr: {string!r}",
+            callme=lambda: next(counter), object=Ephemeral(),
+            number=7, string="hello",
+        )
+
+        flattenEvent(event1)
+
+        event2 = dict(event1)
+        del event2["callme"]
+        del event2["object"]
+        event3 = json.loads(json.dumps(event2))
+        self.assertEquals(formatEvent(event3),
+                          u"callable: 0 attribute: value numrepr: 7 "
+                          "strrepr: 'hello'")
+
+
+    def test_formatFlatEventWithMutatedFields(self):
+        """
+        L{formatEvent} will prefer the stored C{str()} or C{repr()} value for
+        an object, in case the other version.
+        """
+        class unpersistable(object):
+            destructed = False
+
+            def selfDestruct(self):
+                self.destructed = True
+
+            def __repr__(self):
+                if self.destructed:
+                    return "post-serialization garbage"
+                else:
+                    return "un-persistable"
+
+        up = unpersistable()
+        event1 = dict(
+            log_format="unpersistable: {unpersistable}", unpersistable=up
+        )
+
+        flattenEvent(event1)
+        up.selfDestruct()
+
+        self.assertEquals(formatEvent(event1), "unpersistable: un-persistable")
+
+
+    def test_keyFlattening(self):
+        """
+        Test that L{KeyFlattener.flatKey} returns the expected keys for format
+        fields.
+        """
+
+        def keyFromFormat(format):
+            for (
+                literalText,
+                fieldName,
+                formatSpec,
+                conversion,
+            ) in aFormatter.parse(format):
+                return KeyFlattener().flatKey(fieldName, formatSpec,
+                                              conversion)
+
+        # No name
+        try:
+            self.assertEquals(keyFromFormat("{}"), "!:")
+        except ValueError:
+            if sys.version_info[:2] == (2, 6):
+                # In python 2.6, an empty field name causes Formatter.parse to
+                # raise ValueError.
+                pass
+            else:
+                # In Python 2.7, it's allowed, so this exception is unexpected.
+                raise
+
+        # Just a name
+        self.assertEquals(keyFromFormat("{foo}"), "foo!:")
+
+        # Add conversion
+        self.assertEquals(keyFromFormat("{foo!s}"), "foo!s:")
+        self.assertEquals(keyFromFormat("{foo!r}"), "foo!r:")
+
+        # Add format spec
+        self.assertEquals(keyFromFormat("{foo:%s}"), "foo!:%s")
+        self.assertEquals(keyFromFormat("{foo:!}"), "foo!:!")
+        self.assertEquals(keyFromFormat("{foo::}"), "foo!::")
+
+        # Both
+        self.assertEquals(keyFromFormat("{foo!s:%s}"), "foo!s:%s")
+        self.assertEquals(keyFromFormat("{foo!s:!}"), "foo!s:!")
+        self.assertEquals(keyFromFormat("{foo!s::}"), "foo!s::")
+        [keyPlusLiteral] = aFormatter.parse("{x}")
+        key = keyPlusLiteral[1:]
+        sameFlattener = KeyFlattener()
+        self.assertEquals(sameFlattener.flatKey(*key), "x!:")
+        self.assertEquals(sameFlattener.flatKey(*key), "x!:/2")
+
+
+    def test_formatFlatEvent_fieldNamesSame(self):
+        """
+        The same format field used twice is rendered twice.
+        """
+        counter = count()
+
+        class CountStr(object):
+            def __str__(self):
+                return str(next(counter))
+
+        event = dict(
+            log_format="{x} {x}",
+            x=CountStr(),
+        )
+        flattenEvent(event)
+        self.assertEquals(formatEvent(event), u"0 1")
+
+
+    def test_extractField(self, flattenFirst=lambda x: x):
+        """
+        L{extractField} will extract a field used in the format string.
+        """
+        class ObjectWithRepr(object):
+            def __repr__(self):
+                return "repr"
+
+        class Something(object):
+            def __init__(self):
+                self.number = 7
+                self.object = ObjectWithRepr()
+
+            def __getstate__(self):
+                raise NotImplementedError("Just in case.")
+
+        event = dict(
+            log_format="{something.number} {something.object}",
+            something=Something(),
+        )
+
+        flattened = flattenFirst(event)
+
+        def extract(field):
+            return extractField(field, flattened)
+
+        self.assertEquals(extract("something.number"), 7)
+        self.assertEquals(extract("something.number!s"), "7")
+        self.assertEquals(extract("something.object!s"), "repr")
+
+
+    def test_extractFieldFlattenFirst(self):
+        """
+        L{extractField} behaves identically if the event is explicitly
+        flattened first.
+        """
+        def flattened(evt):
+            flattenEvent(evt)
+            return evt
+        self.test_extractField(flattened)
+
+
+    def test_flattenEventWithoutFormat(self):
+        """
+        L{flattenEvent} will do nothing to an event with no format string.
+        """
+        inputEvent = {'a': 'b', 'c': 1}
+        flattenEvent(inputEvent)
+        self.assertEquals(inputEvent, {'a': 'b', 'c': 1})
+
+
+    def test_flattenEventWithInertFormat(self):
+        """
+        L{flattenEvent} will do nothing to an event with a format string that
+        contains no format fields.
+        """
+        inputEvent = {'a': 'b', 'c': 1, 'log_format': 'simple message'}
+        flattenEvent(inputEvent)
+        self.assertEquals(
+            inputEvent,
+            {
+                'a': 'b',
+                'c': 1,
+                'log_format': 'simple message',
+            }
+        )
