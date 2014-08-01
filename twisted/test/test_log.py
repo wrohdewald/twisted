9259e481a20309da20aff55d7f23db2042683f7b
wsanchez <wsanchez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-08-01 17:22:08 +0000

    Make this test a bit stricter: "ERROR:" should be at the beginning of the output.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/log-err-default-7545@42925 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_log.py
+++ twisted/test/test_log.py
@@ -699,7 +699,12 @@ class PythonLoggingObserverTestCase(unittest.SynchronousTestCase):
         """
         f = failure.Failure(ValueError("That is bad."))
         self.lp.msg(failure=f, isError=True)
-        self.assertIn(b"ERROR", self.out.getvalue())
+        prefix = "ERROR:"
+        output = self.out.getvalue()
+        self.assertTrue(
+            output.startswith(b"ERROR:"),
+            "Does not start with {0!r}: {1!r}".format(prefix, output)
+        )
 
 
     def test_formatString(self):
